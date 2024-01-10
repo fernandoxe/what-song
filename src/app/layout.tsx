2798c9e99b5e21808182e1112a782b2c295631bf
Fernando Parra <fernandoxe@gmail.com>
2024-01-10 03:51:06 +0100

    chore: fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,4 @@
 import './globals.css';
-import { Inter } from 'next/font/google';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
@@ -14,9 +11,12 @@ export default function RootLayout({
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content={process.env.NEXT_PUBLIC_SITE_DESCRIPTION} />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin='' />
+        <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Open+Sans:ital,wght@0,400;0,600;1,400&display=swap" rel="stylesheet" />
         <title>{process.env.NEXT_PUBLIC_SITE_TITLE}</title>
       </head>
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   );
 }
