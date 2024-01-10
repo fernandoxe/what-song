7ff10c4f67b458370c7375ba22562cb05b3fcd92
Fernando Parra <fernandoxe@gmail.com>
2024-01-10 03:52:46 +0100

    chore: constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,9 @@
-export const ROUNDS = 3;
+export const ROUNDS = 13;
+
+export const LEVELS = [
+  {code: 'chorus', name: 'chorus', difficulty: 'easy'},
+  {code: 'bridge', name: 'bridge', difficulty: 'medium'},
+  {code: 'verse', name: 'verse', difficulty: 'hard'},
+  {code: 'line', name: 'line', difficulty: 'very hard'},
+  {code: 'words', name: '5 words', difficulty: 'insane'},
+];
