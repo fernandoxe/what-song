526e094990fc4ea3736545ac35d4725057a68331
Fernando Parra <fernandoxe@gmail.com>
2024-01-10 03:54:59 +0100

    chore: results

--- src/components/Results/index.tsx
+++ src/components/Results/index.tsx
@@ -1,15 +1,84 @@
+import { useState } from 'react';
+import { formatTime } from '@/services';
+import { Switch } from '../Switch';
+import { Share } from '@/components/Share';
+import { LEVELS } from '@/constants';
+
 export interface ResultsProps {
-  results: {track: string, correct: boolean}[];
+  level: number;
+  results: {
+    track: string,
+    answer: string,
+    correct: boolean,
+    time: number
+  }[];
 };
 
-export const Results = ({results}: ResultsProps) => {
+export const Results = ({level, results}: ResultsProps) => {
+  const [showYourAnswers, setShowYourAnswers] = useState(false);
+
   return (
-    <div className="text-center">
-      <ul>
-        {results.map((result, index) => (
-          <li key={index}>{result.track} {result.correct ? '✅' : '❌'}</li>
-        ))}
-      </ul>
+    <div className="w-full flex flex-col gap-4">
+      <div className="flex flex-col items-center gap-2">
+        <h2 className="text-lg font-semibold">
+          What {process.env.NEXT_PUBLIC_SITE_ARTIST_SHORT_NAME}&apos;s song is it?
+        </h2>
+        <div>
+          <h3 className="font-semibold">Level: {LEVELS[level].name} ({LEVELS[level].difficulty})</h3>
+        </div>
+      </div>
+      <div className="flex flex-col gap-2">
+        <div className="flex justify-between">
+          <div className="font-semibold">
+            {results.filter(result => result.correct).length} / {results.length} correct
+          </div>
+          <div className="text-sm">
+            Time: {formatTime(results.reduce((acc, result) => acc + result.time, 0))}s
+          </div>
+        </div>
+        <div className="flex flex-col gap-4 text-center">
+          <ul className="flex flex-col gap-0.5">
+            {results.map((result, index) => (
+              <li
+                key={index}
+                className="w-full h-10 px-4 flex items-center justify-between gap-4 bg-purple-50 leading-none"
+              >
+                <div className="text-left flex flex-col whitespace-nowrap overflow-hidden">
+                  <div className={`italic text-sm ${result.correct ? 'text-green-600' : 'text-red-600'} overflow-hidden text-ellipsis`}>
+                    {result.track}
+                  </div>
+                  {showYourAnswers && !result.correct &&
+                    <div className="text-xs overflow-hidden text-ellipsis">
+                      My answer: {result.answer}
+                    </div>
+                  }
+                </div>
+                <div className="text-xs">
+                  {formatTime(result.time)}s
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+      <div className="flex items-center justify-between">
+        <div>
+          <Share
+            results={results}
+            showYourAnswers={showYourAnswers}
+            level={level}
+            onClick={() => console.log('share')}
+          >
+            Share
+          </Share>
+        </div>
+        <Switch
+          onClick={() => setShowYourAnswers(!showYourAnswers)}
+          checked={showYourAnswers}
+        >
+          Show my answers
+        </Switch>
+      </div>
     </div>
   );
 };
