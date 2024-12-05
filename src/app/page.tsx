000e0467c153e203cb50950a459a2201078a107e
Fernando Parra <fernandoxe@gmail.com>
2024-12-05 02:38:19 +0100

    chore: easter egg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 import {Home as HomeComponent} from '../components/Home';
+import Image from 'next/image';
+import uwuImg from '../images/uwu.gif';
 
-export default function Home() {
+export default async function Home({searchParams}: {searchParams: Promise<{ [key: string]: string | string[] | undefined }>}) {
+  const { uwu } = await searchParams;
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main
+      className="flex min-h-screen flex-col items-center justify-between relative"
+    >
+      {uwu === 'true' &&
+        <Image className="bg-img" src={uwuImg} alt="Uwu" />
+      }
       <HomeComponent />
     </main>
   );
